1172cbbc96d2223721abf769d94c5fc1d1095555
prodhanm <mustafizur.prodhan@gmail.com>
2023-10-07 05:56:27 -0400

    created email program and variables.

---
+++ email_addr.py
@@ -0,0 +1,8 @@
+email_from = 'mustafa.prodhan2@gmail.com'
+email_to = 'mustafizur.prodhan@gmail.com'
+email_cc = 'mustafa.prodhan.317@gmail.com'
+email_bcc = [
+    'sabina.yasmin.1010@gmail.com',
+    'pmizanur@gmail.com',
+    'sultanaprodhan2@gmail.com'
+    ]
---
+++ pro_email.py
@@ -0,0 +1,30 @@
+import smtplib
+from email.message import EmailMessage
+from dotenv import dotenv_values
+from string import Template
+from pathlib import Path
+from email_addr import *
+
+config = dotenv_values(".env")
+
+#html = Template(Path("index.html").read_text())
+email = EmailMessage()
+
+email['from'] = email_from
+email['to'] = email_to
+email['cc'] = email_cc
+
+email['bcc'] = [mail for mail in email_bcc]
+
+email['subject'] = 'Good Day!!'
+
+#email.set_content(html.substitute(name="Mufassa"), 'html')
+email.set_content('Test')
+
+with smtplib.SMTP(host=config["HOST"], \
+                  port=config["PORT"]) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login(config['USERNAME'], config['PASSWORD'])
+    smtp.send_message(email)
+    print('done')
